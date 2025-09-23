e85bfbb73aa81ab18f8ebb4cff4b6e5e8e325cf1
Jerry <jerrylalrinmawia@gmail.com>
2025-09-23 19:51:50 +0530

    Adjusted VDOM and applied GSAP animations

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,20 +1,78 @@
 import "./HeroSection.css";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 export default function HeroSection() {
+  const sectionRef = useRef(null);
+
   const divCount = 12;
   const divs = Array.from({ length: divCount }, (_, index) => (
-    <div key={index} className="lines">
-    </div>
+    <div key={index} className="lines"></div>
   ));
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const tl = gsap.timeline();
+
+      tl.from(".heading-content",
+        {
+          duration: 3,
+          opacity:0
+        }
+      )
+      tl.to(
+        ".lines-bg .lines:nth-child(4n+1), .lines-bg .lines:nth-child(4n+2), .lines-bg .lines:nth-child(3), .lines-bg .lines:nth-child(7)",
+        {
+          scaleY: 1,
+          duration: 1,
+          stagger: 0.2,
+          ease: "expoScale",
+        },
+        0 
+      );
+
+      tl.to(
+        ".hero-heading .border.top, .hero-heading .border.bottom",
+        {
+          scaleX: 1,
+          duration: 3,
+          ease: "expoScale",
+        },
+        0
+      );
+
+      tl.to(
+        ".hero-heading h1 .border.right",
+        {
+          scaleY: 1,
+          duration: 1,
+          ease: "expoScale",
+        },
+        1
+      );
+
+      tl.from(".grey-block", {
+        duration: 2,
+        x: "100vw",
+        ease: "back",
+      }, 0);
+    }, sectionRef);
+
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <>
-      <section className="HeroSection">
-        <div className="lines-bg">{divs}</div>
-        <div className="hero-heading">
-          <h1>Navkar Metal</h1>
-          <div className="grey-block"></div>
-        </div>
-      </section>
-    </>
+    <section className="HeroSection" ref={sectionRef}>
+      <div className="lines-bg">{divs}</div>
+      <div className="hero-heading">
+        <span className="border top"></span>
+        <span className="border bottom"></span>
+        <h1 className="heading-content">
+          Navkar Metal
+          <span className="border right"></span>
+        </h1>
+        <div className="grey-block"></div>
+      </div>
+    </section>
   );
 }
