a26a3355c17cc0e77832be915f169d979dac2e85
Jerry <jerrylalrinmawia@gmail.com>
2025-10-20 00:18:51 +0530

    Marquee javascript file

---
+++ public/products/marquee.js
@@ -0,0 +1,31 @@
+const icons = {
+  verified:
+    '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z"/></svg>',
+  shieldCheck:
+    '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm-2 16l-4-4 1.41-1.41L10 14.17l6.59-6.59L18 9l-8 8z"/></svg>',
+  mobile:
+    '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M17 1.01L7 1c-1.1 0-2 .9-2 2v18c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V3c0-1.1-.9-1.99-2-1.99zM17 19H7V5h10v14z"/></svg>',
+  chat: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H6l-2 2V4h16v12z"/></svg>',
+  heart:
+    '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>',
+  box: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zM7 10h2v7H7zm4-3h2v10h-2zm4 6h2v4h-2z"/></svg>',
+};
+
+const banners = [
+  { icon: icons.verified, text: "Affordable pricing with no hidden fees" },
+  { icon: icons.shieldCheck, text: "FDA-regulated pharmacies" },
+  { icon: icons.mobile, text: "100% online process" },
+  { icon: icons.chat, text: "Unlimited provider messaging" },
+  { icon: icons.verified, text: "US-Sourced ingredients" },
+  { icon: icons.heart, text: "Free Shipping Over $50" },
+  { icon: icons.box, text: "New Arrivals Just Dropped!" },
+];
+
+const scrollingContent = [...banners, ...banners];
+
+const marqueeEl = document.getElementById("marqueeContent");
+scrollingContent.forEach((item) => {
+  const span = document.createElement("span");
+  span.innerHTML = item.icon + " " + item.text + "&nbsp;&nbsp;";
+  marqueeEl.appendChild(span);
+});
